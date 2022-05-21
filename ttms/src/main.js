ef1f468e67eaae181070e799bd75faeb38021761
zj <17791893540@qq.com>
2022-05-22 00:05:19 +0800

    update first_page

--- ttms/src/main.js
+++ ttms/src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
 import './assets/css/golabl.css'
+import axios from 'axios'
+
+Vue.prototype.$http = axios
+axios.defaults.baseURL = 'http://127.0.0.1:8080/api/'
 Vue.config.productionTip = false
 
 new Vue({
--- ttms/src/views/HomePage.vue
+++ ttms/src/views/HomePage.vue
@@ -1,29 +1,35 @@
 <template>
     <el-container>
-        
-            <el-header>
 
-                <el-menu :default-active="url1" class="el-menu-demo" mode="horizontal">
-                    <el-menu-item :index="url1" class="left1">首页</el-menu-item>
-                    <el-menu-item :index="url2" class="left1">电影</el-menu-item>
-                    <el-menu-item :index="url3" class="left1">榜单</el-menu-item>
+        <el-header>
 
-                    <el-dropdown>
-                        <img class="img" src="https://p0.meituan.net/movie/7dd82a16316ab32c8359debdb04396ef2897.png" alt="">
-                        <el-dropdown-menu slot="dropdown">
-                            <el-dropdown-item>登录</el-dropdown-item>
+            <el-menu :default-active="url1" class="el-menu-demo" mode="horizontal">
+                <el-menu-item :index="url1" class="left1">首页</el-menu-item>
+                <el-menu-item :index="url2" class="left1">电影</el-menu-item>
+                <el-menu-item :index="url3" class="left1">榜单</el-menu-item>
 
-                        </el-dropdown-menu>
-                    </el-dropdown>
+                <el-dropdown>
+                    <img class="img" src="https://p0.meituan.net/movie/7dd82a16316ab32c8359debdb04396ef2897.png" alt="">
+                    <el-dropdown-menu slot="dropdown">
+                        <el-dropdown-item >
+                            <p  v-if="IsLogin===false"><a @click="login">登录</a></p>
+                            
+                        </el-dropdown-item>
+                         <el-dropdown-item >
+                            <p><a @click="logout">退出</a></p>
+                         </el-dropdown-item>
+                    </el-dropdown-menu>
+                </el-dropdown>
 
-                </el-menu>
-            </el-header>
-        
+            </el-menu>
+        </el-header>
+
         <el-main>
             <router-view></router-view>
         </el-main>
-        
+
     </el-container>
+
 </template>
 
 <script>
@@ -36,10 +42,27 @@ export default {
             url1: "FisrtPage",
             url2: "borad",
             url3: "movie",
+            IsLogin: false,
+            token:''
         }
     },
+    created() {
+        this.token = window.sessionStorage.getItem('token');
+        if (this.token===null) {
+            this.IsLogin = false
+        } else {
+            this.IsLogin = true
+        }
+        //alert(this.token);
+    },
     methods: {
-
+        login() {
+            this.$router.push('/login');
+        },
+        logout() {
+            window.sessionStorage.removeItem('token');
+            this.IsLogin = false
+        }
     }
 
 }
@@ -72,5 +95,4 @@ export default {
     margin-top: 10px;
     float: right;
 }
-
 </style>
