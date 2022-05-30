cff8efcb01a3b29ea57b88d56de65ad33b6760fc
zj <17791893540@qq.com>
2022-05-31 00:07:07 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import './plugins/element.js'
+import './assets/css/golabl.css'
+import axios from 'axios'
+
+Vue.config.productionTip = false
+
+Vue.prototype.$http = axios
+ axios.defaults.baseURL = 'http://127.0.0.1:8080/api/'
+//axios.defaults.baseURL = 'http://124.70.199.139:8080/api/'
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
