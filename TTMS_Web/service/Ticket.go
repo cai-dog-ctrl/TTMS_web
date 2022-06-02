54ac4096f5794ab651d6628555aff3c5cda74dc6
yclchuxue <3136165538@qq.com>
2022-06-02 08:46:36 +0800

    update ticket

--- TTMS_Web/service/Ticket.go
+++ TTMS_Web/service/Ticket.go
@@ -4,6 +4,7 @@ import (
 	"TTMS/dao/mysql"
 	"TTMS/models"
 	"TTMS/pkg/utils"
+	"strings"
 )
 
 //有关票务的业务逻辑代码
@@ -30,4 +31,56 @@ func GetTicketByScheduleId(id int64) ( *models.TickRets, error) {
 	return p, nil	
 }
 
+func GetTicketByMovieIdAndDateDay(movie_id, date_day string) (*models.TickRets, error) {
+	
+	id := utils.ShiftToNum64(movie_id)
+	
+	str_day  := strings.Split(date_day, "-")
+	day := str_day[0] + str_day[1] + str_day[2] 
+	
+	p1, err := mysql.GetTicketByMovieIdAndDateDay(id, utils.ShiftToNum64(day))
+	if err != nil {
+		return nil, err
+	}
 
+	p := new(models.TickRets)
+	p.Total = p1.Total
+	for _,it := range p1.List {
+		p.List = append(p.List, models.TickRet{
+			Id: utils.ShiftToStringFromInt64(it.Id),
+			ScheduleId: utils.ShiftToStringFromInt64(it.ScheduleId),
+			CinemaId: utils.ShiftToStringFromInt64(it.CinemaId),
+			MovieId: utils.ShiftToStringFromInt64(it.MovieId),
+			SeatId: utils.ShiftToStringFromInt64(it.SeatId),
+			Status:  it.Status,
+		})
+	}
+	return p, nil
+}	
+
+func GetTicketByCinemaIdAndDateDay(cinema_id, date_day string) (*models.TickRets, error) {
+	id := utils.ShiftToNum64(cinema_id)
+
+	str_day  := strings.Split(date_day, "-")
+	day := str_day[0] + str_day[1] + str_day[2]
+
+	p1, err := mysql.GetTicketByCinemaIdAndDateDay(id, utils.ShiftToNum64(day))
+
+	if err != nil {
+		return nil, err
+	}
+
+	p := new(models.TickRets)
+	p.Total = p1.Total
+	for _,it := range p1.List {
+		p.List = append(p.List, models.TickRet{
+			Id: utils.ShiftToStringFromInt64(it.Id),
+			ScheduleId: utils.ShiftToStringFromInt64(it.ScheduleId),
+			CinemaId: utils.ShiftToStringFromInt64(it.CinemaId),
+			MovieId: utils.ShiftToStringFromInt64(it.MovieId),
+			SeatId: utils.ShiftToStringFromInt64(it.SeatId),
+			Status:  it.Status,
+		})
+	}
+	return p, nil
+}
