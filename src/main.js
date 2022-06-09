831fb5b2108aa61bd5a88f0ecf205cdc22dbd526
cai-dog-ctrl <1296643805@qq.com>
2022-06-09 09:28:05 +0800

    bug

--- src/main.js
+++ src/main.js
@@ -10,6 +10,10 @@ Vue.config.productionTip = false
 Vue.prototype.$http = axios
  axios.defaults.baseURL = 'http://127.0.0.1:8080/api/'
 //axios.defaults.baseURL = 'http://124.70.199.139:8080/api/'
+axios.interceptors.request.use(config=>{  //请求拦截器（在请求之前进行一些配置）
+  config.headers.Authorization=window.sessionStorage.getItem('token')
+  return config
+})
 new Vue({
   router,
   render: h => h(App)
